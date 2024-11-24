751e607d6f9471cfccb12840ee6ebcd22738b025
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2024-11-24 22:29:01 +0100

    Add async let

--- StructuredProgramming/StructuredProgramming.playground/Contents.swift
+++ StructuredProgramming/StructuredProgramming.playground/Contents.swift
@@ -5,3 +5,55 @@ import Foundation
  https://developer.apple.com/videos/play/wwdc2021/10134/
  */
 
+final class Fetcher {
+    
+    func fetchDate() async -> Date {
+        let timeout = UInt32.random(in: 2...4)
+        sleep(timeout)
+        
+        return Date()
+    }
+
+    func fetchMeta(date: Date) async -> String {
+        let timeout = UInt32.random(in: 1...3)
+        sleep(timeout)
+        
+        return date.description
+    }
+}
+
+final class Sample {
+    
+    
+    /// Plain async await
+    func fetch_0() async {
+        let fetcher = Fetcher()
+        
+        let date = await fetcher.fetchDate()
+        let meta = await fetcher.fetchMeta(date: date)
+        
+        print(date.timeIntervalSince1970)
+        print(meta)
+    }
+    
+    /// Async let
+    func fetch_1() async {
+        let fetcher = Fetcher()
+        
+        async let date = fetcher.fetchDate()
+        /**
+         Perform taks until result needed
+         */
+        await print(date.timeIntervalSince1970)
+        
+        let meta = await fetcher.fetchMeta(date: date)
+        print(meta)
+    }
+}
+
+Task {
+    let sample = Sample()
+    await sample.fetch_0()
+    await sample.fetch_1()
+}
+
