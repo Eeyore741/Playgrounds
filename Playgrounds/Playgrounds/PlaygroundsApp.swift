41e2335803b51d7fc54b4e3cfc53c1da0147746a
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2025-12-23 01:17:22 +0100

    Add playground app
    Swift 5, strict memory safety = no, strict concurrency checking = minimal, approachable concurrency = yes

---
+++ Playgrounds/Playgrounds/PlaygroundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundsApp.swift
+//  Playgrounds
+//
+//  Created by Vitalii Kuznetsov on 2025-12-22.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
