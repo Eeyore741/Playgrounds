a3dd391f8921cec1112da44443d3ee693a38bbf6
Vitalii Kuznetsov <v-blacksmith@hotmail.com>
2026-01-04 02:40:30 +0100

    S5: Refactor content in single view
    All types are fileprivate now

--- Playgrounds/pg-5-16/pg-5-16App.swift
+++ Playgrounds/pg-5-16/pg-5-16App.swift
@@ -6,9 +6,67 @@
 //
 
 import SwiftUI
+import Combine
+
+// To not to use SwiftUI in data model.
+fileprivate typealias SimpleColor = (Double, Double, Double)
+
+fileprivate func randomSampleColor() -> SimpleColor {
+    (
+        Double(Int.random(in: 0...255)) / 255,
+        Double(Int.random(in: 0...255)) / 255,
+        Double(Int.random(in: 0...255)) / 255,
+    )
+}
+
+fileprivate struct Book {
+    let id: UUID
+    let name: String
+    let color: SimpleColor
+    let rating: Int
+}
+
+fileprivate  protocol BookProvider {
+    func addBook(_ book: Book)
+    func makeSubject() -> CurrentValueSubject<[Book], Never>
+    func loadBooks()
+}
+
+fileprivate actor LocalBookProvider: BookProvider {
+    
+    private(set) var books: [Book] = []
+    
+    func addBook(_ book: Book) {
+        books.append(book)
+    }
+    
+    func makeSubject() -> CurrentValueSubject<[Book], Never> {
+        .init(books)
+    }
+    
+    func loadBooks() {
+        
+    }
+}
+
+fileprivate struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
 
 @main
-struct PlaygroundsApp: App {
+fileprivate struct PlaygroundsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
